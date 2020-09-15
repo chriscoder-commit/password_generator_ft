2a5fef00a8cc741f8841cfe0856c3ed867717bed
Harrison Malone <harrison.malone2@gmail.com>
2020-09-15 12:08:16 +1000

    refactored password generation code into methods

--- lib/app.rb
+++ lib/app.rb
@@ -3,15 +3,24 @@
 # todos
 
 # 1. how do we generate a password in ruby?
-# i want all passwords to be 10 characters in length
 
-password = ""
-10.times do 
-  ascii_decimal = rand(33..126)
-  password = password + ascii_decimal.chr
+def ask_user_for_password_length
+  puts "How many characters would your like your password to be?"
+  print "> "
+  gets.chomp.to_i
 end
 
-puts password
+def generate_password
+  password_length = ask_user_for_password_length
+  password = ""
+  password_length.times do 
+    ascii_decimal = rand(33..126)
+    password += ascii_decimal.chr
+  end
+  return password
+end
+
+puts generate_password
 
 # 2. how will the user ask for a new password?
 # 3. how will we store the passwords in memory?
