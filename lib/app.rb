ff7112e2b43be489c9963a2d72897989e1a49a18
Harrison Malone <harrison.malone2@gmail.com>
2020-09-15 12:26:18 +1000

    added loop for keeping user in app and added menus for selections

--- lib/app.rb
+++ lib/app.rb
@@ -2,7 +2,7 @@
 
 # todos
 
-# 1. how do we generate a password in ruby?
+# 1. how do we generate a password in ruby? ✅
 
 def ask_user_for_password_length
   puts "How many characters would your like your password to be?"
@@ -20,8 +20,31 @@ def generate_password
   return password
 end
 
-puts generate_password
+def menu 
+  puts "Welcome to the password generator!"
+  puts "What would you like to do? (type 1 or 2)"
+  puts "1. Generate new password"
+  puts "2. Exit"
+  print "> "
+end
 
-# 2. how will the user ask for a new password?
-# 3. how will we store the passwords in memory?
-# 4. how many characters will be in the password?
+def user_selection
+  menu 
+  gets.chomp.to_i
+end
+
+loop do 
+  case user_selection
+  when 1 
+    puts generate_password
+  when 2 
+    exit
+  else
+    puts "Wrong user input"
+    puts "Please type a 1 or a 2"
+  end
+end
+
+# 2. how many characters will be in the password? ✅
+# 3. how will the user ask for a new password? 👍
+# 4. how will we store the passwords in memory? ❌
