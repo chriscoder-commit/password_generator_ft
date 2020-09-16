de515a9cc95d127edf67afacf481c0ad896a7539
Harrison Malone <harrison.malone2@gmail.com>
2020-09-16 10:32:56 +1000

    added password database and implemented store_password and view_passwords

--- lib/app.rb
+++ lib/app.rb
@@ -12,30 +12,47 @@ def user_selection
 end
 
 def menu 
-  puts "Welcome to the password generator!"
+  puts "\nWelcome to the password generator!"
   puts "What would you like to do? (type 1 or 2)"
   puts "1. Generate new password"
   puts "2. Exit"
   print "> "
 end
 
-def generate_password
+def view_passwords(password_database)
+  puts ""
+  password_database.each_with_index do |password, index|
+    puts "#{index + 1}. #{password}"
+  end
+end
+
+def store_password(password, password_database)
+  # push the password into my array that will store passwords
+  password_database << password
+  # view all of the passwords
+  view_passwords(password_database)
+end 
+
+def generate_password(password_database)
   password_length = ask_user_for_password_length
   password = ""
   password_length.times do 
     ascii_decimal = rand(33..126)
     password += ascii_decimal.chr
   end
-  password
+  store_password(password, password_database)
 end
 
 # method calling
 # loop logic
 
+# database of passwords
+password_database = []
+
 loop do 
   case user_selection
   when 1 
-    puts generate_password
+    generate_password(password_database)
   when 2 
     exit
   else
@@ -48,4 +65,4 @@ end
 # 1. how do we generate a password in ruby? ✅
 # 2. how many characters will be in the password? ✅
 # 3. how will the user ask for a new password? ✅
-# 4. how will we store the passwords in memory? ❌
+# 4. how will we store the passwords in memory? ✅
